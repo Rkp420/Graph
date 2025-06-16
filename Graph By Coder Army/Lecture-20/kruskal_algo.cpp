5612cf0bfe94b8d26c0aad50adfaedbae8143f08
Rkp420 <rkp20442@gmail.com>
2025-06-17 01:58:40 +0530

    Added Something new

--- Graph By Coder Army/Lecture-20/kruskal_algo.cpp
+++ Graph By Coder Army/Lecture-20/kruskal_algo.cpp
@@ -195,6 +195,101 @@ public:
     }
 };
 
+// Without Tupple
+class Solution
+{
+public:
+    vector<int> parent;
+    vector<int> rank;
+
+    int findParent(int x)
+    {
+        if (x == parent[x])
+            return x;
+
+        return parent[x] = findParent(parent[x]);
+    }
+
+    void Union(int x, int y)
+    {
+        int x_parent = findParent(x);
+        int y_parent = findParent(y);
+
+        if (x_parent == y_parent)
+            return;
+
+        if (rank[x_parent] > rank[y_parent])
+        {
+            parent[y_parent] = x_parent;
+        }
+        else if (rank[x_parent] < rank[y_parent])
+        {
+            parent[x_parent] = y_parent;
+        }
+        else
+        {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+
+        parent.resize(V);
+        rank.resize(V, 0);
+
+        for (int i = 0; i < V; i++)
+        {
+            parent[i] = i;
+        }
+
+        // Define custom comparator for min-heap of vector<int>
+        auto cmp = [](const vector<int> &a, const vector<int> &b)
+        {
+            return a[2] > b[2]; // min-heap based on weight
+        };
+
+        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> minHeap(cmp);
+
+        vector<vector<bool>> visited(V, vector<bool>(V, false));
+
+        for (int u = 0; u < V; u++)
+        {
+            for (auto &nbr : adj[u])
+            {
+                int v = nbr[0];
+                int wt = nbr[1];
+                if (!visited[u][v])
+                {
+                    minHeap.push({u, v, wt});
+                    visited[u][v] = visited[v][u] = true;
+                }
+            }
+        }
+
+        int cost = 0;
+
+        while (!minHeap.empty())
+        {
+            auto edge = minHeap.top();
+            minHeap.pop();
+
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            if (findParent(u) != findParent(v))
+            {
+                Union(u, v);
+                cost += wt;
+            }
+        }
+
+        return cost;
+    }
+};
+
 int main()
 {
     return 0;
